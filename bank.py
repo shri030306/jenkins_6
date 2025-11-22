f982ff38fb713c8b873d790e39e3b08cf0ff60f0
shri030306 <shrilakshmigs03@gmail.com>
2025-11-22 13:06:19 +0530

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python deposit.py <initial_balance> <deposit_amount>")
+    sys.exit(1)
+
+initial_balance = float(sys.argv[1])
+deposit_amount = float(sys.argv[2])
+
+
+updated_balance = initial_balance + deposit_amount
+
+
+print("\n--- Account Details ---")
+print("Initial Balance:", initial_balance)
+print("Deposit Amount:", deposit_amount)
+print("Updated Balance:", updated_balance)
